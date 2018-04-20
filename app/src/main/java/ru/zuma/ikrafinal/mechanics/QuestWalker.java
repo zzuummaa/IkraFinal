c1aedcd1051aec1403e237ffdc850b8491ba6cb4
TimeEscaper <rentgeny05@gmail.com>
2018-04-20 21:45:06 +0300

    Workspaces display added

---
+++ app/src/main/java/ru/zuma/ikrafinal/mechanics/QuestWalker.java
@@ -0,0 +1,16 @@
+package ru.zuma.ikrafinal.mechanics;
+
+import ru.zuma.ikrafinal.model.Quest;
+
+/**
+ * Created by sibirsky on 20.04.18.
+ */
+
+public class QuestWalker {
+
+    private Quest root;
+
+    public QuestWalker(Quest root) {
+        this.root = root;
+    }
+}
